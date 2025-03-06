04b801a46d44fbd9a26b32fa7766f6c21d3b532c
Mohamed_Nihmathullah <mohamednihmath13@gmail.com>
2025-03-06 14:26:36 +0800

    new change

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Clock from "./components/Clock";
 
 function App() {
   return (
-    <div style={{ textAlign: "center", marginTop: "80px"}}>
+    <div style={{ textAlign: "center", marginTop: "100px"}}>
       {/* <h1>Live Digital Clock</h1> */}
       <Clock />
     </div>
