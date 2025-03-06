536deed4ac12b044d85b31a3ed379e3fd0db53cc
Mohamed_Nihmathullah <mohamednihmath13@gmail.com>
2025-03-06 14:28:21 +0800

    just commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
+// import Clock from "./components/Clock";
+
+// function App() {
+//   return (
+//     <div style={{ textAlign: "center", marginTop: "100px"}}>
+//       {/* <h1>Live Digital Clock</h1> */}
+//       <Clock />
+//     </div>
+//   );
+// }
+
+// export default App;
+
 import Clock from "./components/Clock";
 
 function App() {
   return (
-    <div style={{ textAlign: "center", marginTop: "100px"}}>
-      {/* <h1>Live Digital Clock</h1> */}
+    <div style={{
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      height: "100vh",
+      flexDirection: "column",
+      backgroundColor: "#282c34",
+      color: "white"
+    }}>
       <Clock />
     </div>
   );
